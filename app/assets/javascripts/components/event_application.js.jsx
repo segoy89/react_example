b8f0471e08e14a1207a1cf2326f3ccdb251331ff
segoy89 <a.szamansky@gmail.com>
2016-11-27 01:54:36 +0100

    search + searchform

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -1,13 +1,41 @@
 var EventApplication = React.createClass({
+  getInitialState: function(){
+    return { events:[] }
+  },
+  componentDidMount: function(){
+    this.getDataFromApi();
+  },
+  getDataFromApi: function(){
+    var self = this;
+    $.ajax({
+      url: '/api/events',
+      method: 'GET',
+      success: function(data){
+        self.setState({ events: data });
+      },
+      error: function(xhr, status, error){
+        alert('Cannot get data from Api: ', error);
+      }
+    });
+  },
+  handleSearch: function(events) {
+    this.setState({ events: events });
+  },
   render: function() {
     return(
       <div className="container">
         <div className="jumbotron">
           <h1>ReactJS Tutorial</h1>
+          <p>by Piotr Jaworski</p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <SearchForm handleSearch={this.handleSearch} />
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
-            <EventTable />
+            <EventTable events={this.state.events} />
           </div>
         </div>
       </div>
--- app/assets/javascripts/components/event_table.js.jsx
+++ app/assets/javascripts/components/event_table.js.jsx
@@ -1,5 +1,9 @@
 var EventTable = React.createClass({
   render: function() {
+    var events = [];
+    this.props.events.forEach(function(event){
+      events.push(<Event event={event} key={'event'+event.id}/>);
+    }.bind(this));
     return(
       <table className="table table-striped">
         <thead>
@@ -10,7 +14,9 @@ var EventTable = React.createClass({
             <th className="col-md-4">Description</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {events}
+        </tbody>
       </table>
     )
   }
